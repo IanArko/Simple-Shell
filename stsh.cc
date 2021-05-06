a1ee454ba9842fda08f3d38777760ac9290101f9
CS110 tools <cs110@cs.stanford.edu>
2021-05-05 17:05:54 -0700

    This project passes 3 of the 12 default sanity check cases.

--- stsh.cc
+++ stsh.cc
@@ -26,12 +26,29 @@ using namespace std;
 
 static STSHJobList joblist; // the one piece of global data we need so signal handlers can access it
 
+static void waitForFg(){
+  // stop the program to run what is in the foreground
+  // Block all signals except for sigchild
+  sigset_t mask;
+  sigemptyset(&mask);
+  // sigaddset(&mask, SIGCHLD);
+  // Link below contains a sample run that may or may not be an issue. If it isn't delete these lines.
+  // https://www.notion.so/Maybe-Timing-Error-e5f15d7fb6c74829baba2c76113e3dec
+  // sigprocmask ????
+  // cout << "WAIT FOR FG" << endl;
+  // cout << joblist;
+  while(joblist.hasForegroundJob()) {
+    sigsuspend(&mask);
+  }
+}
+
 /**
  * Builtin Handlers
  * -----------------------
  */
 
 static void fgHandler(const pipeline& p){
+  // Get the inputs and do error checking
   char* token0 = p.commands[0].tokens[0];
   char* token1 = p.commands[0].tokens[1];
   
@@ -45,7 +62,41 @@ static void fgHandler(const pipeline& p){
     if(!joblist.containsJob(t0)){
       throw STSHException("fg " + to_string(t0) + ": No such job.");
     } else {
+      // IF there were no errors, get the job with #t0 from the job list
+      STSHJob& job = joblist.getJob(t0);
+      pid_t groupID = job.getGroupID();
+      kill(groupID, SIGCONT);      
+      job.setState(kForeground);
+      /*
+      if (tcsetpgrp(STDIN_FILENO, groupID) < 0) {
+          throw STSHException("Failed to transfer STDIN control to foreground process.");
+      }
+      */
+      waitForFg();
+    }    
+  }
+}
 
+static void bgHandler(const pipeline& p){
+  // Get the inputs and do error checking
+  char* token0 = p.commands[0].tokens[0];
+  char* token1 = p.commands[0].tokens[1];
+
+  int t0 = 0;
+  if (token0 != NULL) {
+    t0 = atoi(token0);
+  }
+  if(t0 < 1 || token1 != NULL){
+    throw STSHException("Usage: bg <jobid>.");
+  } else {
+    if(!joblist.containsJob(t0)){
+      throw STSHException("bg " + to_string(t0) + ": No such job.");
+    } else {
+      // IF there were no errors, get the job with #t0 from the job list
+      STSHJob& job = joblist.getJob(t0);
+      pid_t groupID = job.getGroupID();
+      kill(groupID, SIGCONT);
+      job.setState(kBackground);
     }
   }
 }
@@ -71,12 +122,17 @@ static bool handleBuiltin(const pipeline& pipeline) {
   case 2: // fg
     try {
       fgHandler(pipeline);
-      break;
     } catch (const STSHException& e) {
       cerr << e.what() << endl;
     }
     break;
-  case 3: cout << "Called bg command\n"; break;
+  case 3: // bg
+    try {
+      bgHandler(pipeline);
+    } catch (const STSHException& e) {
+      cerr << e.what() << endl;
+    }
+    break;
   case 4: cout << "Called slay command\n"; break;
   case 5: cout << "Called halt command\n"; break;
   case 6: cout << "Called cont command\n"; break;
@@ -105,11 +161,21 @@ static void updateJobList(STSHJobList& jobList, pid_t pid, STSHProcessState stat
  * sigChildReaps our child process and removes jobs from the job list
  */
 static void sigChild(int sig){
+  // cout << "SIG CHILD HANDLER" << endl;
+  // cout << joblist;
   int status;
   pid_t pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED);
-  if (WIFEXITED(status) || WIFSIGNALED(status)) updateJobList(joblist, pid, kTerminated);
-  if (WIFSTOPPED(status)) updateJobList(joblist, pid, kStopped);
-  if (WIFCONTINUED(status)) updateJobList(joblist, pid, kRunning);
+  if (WIFEXITED(status) || WIFSIGNALED(status)) {
+     updateJobList(joblist, pid, kTerminated);
+  } else if(WIFSTOPPED(status)) {
+     updateJobList(joblist, pid, kStopped);
+  } else { // WIFCONTINUED(status)
+    updateJobList(joblist, pid, kRunning);
+  }
+  if (tcsetpgrp(STDIN_FILENO, getpid()) < 0) {
+      throw STSHException("Failed to transfer STDIN control back to terminal.");
+  }
+  // cout << joblist;
 }
 
 /* Function: sigForward
@@ -154,13 +220,29 @@ static void installSignalHandlers() {
  * Creates a new job on behalf of the provided pipeline.
  */
 static void createJob(const pipeline& p) {
+  int n = p.commands.size();
+  int fds[(n-1)*2];
+  pipe2(fds, O_CLOEXEC);
   STSHJob& job = joblist.addJob(kForeground);
   
-  pid_t child = fork();
-   
-  if (child != 0){
-    job.addProcess(STSHProcess(child, p.commands[0]));
+  if(p.background) {
+    job.setState(kBackground);
+    cout << "[" << to_string(job.getNum()) << "] ";
+  }
+
+  // First Process  
+  pid_t pid0 = fork();
+  if (pid0 != 0){
+      setpgid(pid0, 0);
+    job.addProcess(STSHProcess(pid0, p.commands[0]));    
+    // Print each of the group ids
+    if(p.background) {
+      cout << job.getGroupID();
+    }
   } else {
+    setpgid(getpid(), 0);
+    dup2(fds[1],STDOUT_FILENO);
+
     int n = sizeof(p.commands[0].tokens) / sizeof(p.commands[0].tokens[0]);
     char *combined[n + 1];
     combined[0] = (char *)p.commands[0].command;
@@ -170,17 +252,54 @@ static void createJob(const pipeline& p) {
     }
 
     execvp(combined[0], combined);
+    close(fds[0]);
+    close(fds[1]);
     exit(0);
   }
   
+  // Middle proccesses  
 
-  // stop the program to run what is in the foreground
-  // Block all signals except for sigchild
-  sigset_t mask;
-  sigemptyset(&mask);
-  // sigprocmask ????
-  while(joblist.hasForegroundJob()) {
-    sigsuspend(&mask);
+
+  // Last Process
+  if (n > 1){ 
+  pid_t pidl = fork();
+
+  if (pidl != 0){
+    setpgid(pidl, pid0);
+    job.addProcess(STSHProcess(pidl, p.commands[1]));
+
+    // Print each of the group ids
+    if(p.background) {
+      cout << " " << pidl << endl;
+    }
+  } else {
+    setpgid(getpid(), pid0);
+    dup2(fds[0],STDIN_FILENO);
+
+    char *combined[kMaxArguments + 1];
+    combined[0] = (char *)p.commands[1].command;
+
+    for (int i=1; i < (kMaxArguments + 1); i++) {
+        combined[i] = (char *)p.commands[1].tokens[i-1];
+    }
+
+    execvp(combined[0], combined);
+    close(fds[0]);
+    close(fds[1]);
+    exit(0);
+  }
+  }
+  close(fds[0]);
+  close(fds[1]);
+
+  // Run fg proccess in fg
+  if(!p.background) {
+    /*
+    if (tcsetpgrp(STDIN_FILENO, child) < 0) {
+      throw STSHException("Failed to transfer STDIN control to foreground process.");
+    }
+    */
+    waitForFg();
   }
 }
 
